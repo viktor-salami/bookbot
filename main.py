20334cc3c6d9479faf8fa01420c7934817df882c
viktor-salami <vikorsalami@gmail.com>
2025-11-15 12:19:53 -0500

    fix bugs

--- main.py
+++ main.py
@@ -10,36 +10,35 @@ def get_book_text (path):
 def character_read_out (charList):
     for entry in charList:
         currentDict = entry
-        print(f"{currentDict["char"]}: {currentDict["num"]}")
+        print(f"{currentDict['char']}: {currentDict['num']}")
 
 def main ():
-    try:
-        with open(sys.argv[1]) as book:
-            # Get full text of file.
-            text = get_book_text(book)
-
-            # Get total word count.
-            wordCount = count_words(text)
-
-            # Get count of all characters as dictionary.
-            charCount = count_non_words(text)
-
-            # Sort charCount dictionary into a list of seperate dictionaries.
-            sortedChars = sort_chars(charCount)
-
-        print(
-            "============ BOOKBOT ============\n" + 
-            f"Analyzing book found at {book.name}...\n" + 
-            "----------- Word Count ----------\n"
-            f"Found {wordCount} total words\n" + 
-            "--------- Character Count -------\n"
-            )
-        print(character_read_out(sortedChars))
-        print("============= END ===============")
-    except:
+    if len(sys.argv) != 2:
         print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
 
-    #print(f"Found {wordCount} total words")
-    #print(sort_chars(charCount))
+
+    with open(sys.argv[1]) as book:
+        # Get full text of file.
+        text = get_book_text(book)
+
+        # Get total word count.
+        wordCount = count_words(text)
+
+        # Get count of all characters as dictionary.
+        charCount = count_non_words(text)
+
+        # Sort charCount dictionary into a list of seperate dictionaries.
+        sortedChars = sort_chars(charCount)
+
+    print(
+        "============ BOOKBOT ============\n" + 
+        f"Analyzing book found at {book.name}...\n" + 
+        "----------- Word Count ----------\n"
+        f"Found {wordCount} total words\n" + 
+        "--------- Character Count -------\n"
+        )
+    character_read_out(sortedChars)
+    print("============= END ===============")
 
 main()
