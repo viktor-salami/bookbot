f2614393da8e8b679ef433f6952bc3526f2b34dd
viktor-salami <vikorsalami@gmail.com>
2025-11-15 11:27:08 -0500

    add user book selection

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import sys
 from stats import count_words
 from stats import count_non_words
 from stats import sort_chars
@@ -12,28 +13,31 @@ def character_read_out (charList):
         print(f"{currentDict["char"]}: {currentDict["num"]}")
 
 def main ():
-    with open("books/frankenstein.txt") as book:
-        # Get full text of file.
-        text = get_book_text(book)
+    try:
+        with open(sys.argv[1]) as book:
+            # Get full text of file.
+            text = get_book_text(book)
 
-        # Get total word count.
-        wordCount = count_words(text)
+            # Get total word count.
+            wordCount = count_words(text)
 
-        # Get count of all characters as dictionary.
-        charCount = count_non_words(text)
+            # Get count of all characters as dictionary.
+            charCount = count_non_words(text)
 
-        # Sort charCount dictionary into a list of seperate dictionaries.
-        sortedChars = sort_chars(charCount)
+            # Sort charCount dictionary into a list of seperate dictionaries.
+            sortedChars = sort_chars(charCount)
 
-    print(
-        "============ BOOKBOT ============\n" + 
-        f"Analyzing book found at {book.name}...\n" + 
-        "----------- Word Count ----------\n"
-        f"Found {wordCount} total words\n" + 
-        "--------- Character Count -------\n"
-          )
-    print(character_read_out(sortedChars))
-    print("============= END ===============")
+        print(
+            "============ BOOKBOT ============\n" + 
+            f"Analyzing book found at {book.name}...\n" + 
+            "----------- Word Count ----------\n"
+            f"Found {wordCount} total words\n" + 
+            "--------- Character Count -------\n"
+            )
+        print(character_read_out(sortedChars))
+        print("============= END ===============")
+    except:
+        print("Usage: python3 main.py <path_to_book>")
 
     #print(f"Found {wordCount} total words")
     #print(sort_chars(charCount))
