5c812237732a14f3268635af8f0331c6c41d50a5
viktor-salami <vikorsalami@gmail.com>
2025-11-15 10:47:37 -0500

    add human readable output

--- main.py
+++ main.py
@@ -1,16 +1,41 @@
 from stats import count_words
 from stats import count_non_words
+from stats import sort_chars
 
 def get_book_text (path):
     contents = path.read()
     return contents
 
+def character_read_out (charList):
+    for entry in charList:
+        currentDict = entry
+        print(f"{currentDict["char"]}: {currentDict["num"]}")
+
 def main ():
     with open("books/frankenstein.txt") as book:
+        # Get full text of file.
         text = get_book_text(book)
+
+        # Get total word count.
         wordCount = count_words(text)
+
+        # Get count of all characters as dictionary.
         charCount = count_non_words(text)
-    print(f"Found {wordCount} total words")
-    print(f"Here is the frequency of the following characters: {charCount}")
+
+        # Sort charCount dictionary into a list of seperate dictionaries.
+        sortedChars = sort_chars(charCount)
+
+    print(
+        "============ BOOKBOT ============\n" + 
+        f"Analyzing book found at {book.name}...\n" + 
+        "----------- Word Count ----------\n"
+        f"Found {wordCount} total words\n" + 
+        "--------- Character Count -------\n"
+          )
+    print(character_read_out(sortedChars))
+    print("============= END ===============")
+
+    #print(f"Found {wordCount} total words")
+    #print(sort_chars(charCount))
 
 main()
--- stats.py
+++ stats.py
@@ -16,3 +16,29 @@ def count_non_words (text):
             charCount[char] = 1
 
     return charCount
+
+def sort_on (items):
+    return items["num"]
+
+
+def sort_chars (charDict):
+    sortedChars = []
+    
+    # create a list of dictionaries where each dict contains two key value pairs:
+    # one for the character (alphanumeric only), one for its frequency.
+    # I.E. [{char: 'e', num: 500}, {char: 'c', num: 250}]
+    for char, count in charDict.items():
+        currentDict = {}
+        currentDict[char] = (count)
+        for key, value in currentDict.items():
+            charNum = {}
+            if key.isalpha():
+                charNum["char"] = key
+                charNum["num"] = value
+                sortedChars.append(charNum)
+            else:
+                continue
+    
+    sortedChars.sort(reverse=True, key=sort_on)
+    return sortedChars
+
